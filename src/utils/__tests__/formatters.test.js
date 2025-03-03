c842d4ad23aad93a7d387e4e3ac597dbea113ed3
Logan Han <logan@han.life>
2025-03-03 15:33:32 +1100

    add tests for formatters

--- src/utils/__tests__/formatters.test.js
+++ src/utils/__tests__/formatters.test.js
@@ -1,4 +1,4 @@
-import { formatABN, formatACN, formatBSB } from '../formatters';
+import { formatABN, formatACN, formatBSB, formatPhoneNumber } from '../formatters';
 
 test('formats ABN correctly', () => {
     expect(formatABN('12345678901')).toBe('12 345 678 901');
@@ -16,4 +16,11 @@ test('formats BSB correctly', () => {
     expect(formatBSB('123456')).toBe('123-456');
     expect(formatBSB('123-456')).toBe('123-456');
     expect(formatBSB('1234')).toBe('1234');
+});
+
+test('formats phone number correctly', () => {
+    expect(formatPhoneNumber('0412345678')).toBe('0412 345 678');
+    expect(formatPhoneNumber('02 1234 5678')).toBe('02 1234 5678');
+    expect(formatPhoneNumber('+61 412 345 678')).toBe('+61 412 345 678');
+    expect(formatPhoneNumber('1234')).toBe('1234');
 });
